3ea70a21d488c61308f07a857294a1b637861374
pan9 <zrking_sun@163.com>
2021-08-25 00:02:49 +0800

    feat: add intelligent_data extension

--- src/extensions/di_intelligent_data/index.js
+++ src/extensions/di_intelligent_data/index.js
@@ -1,151 +1,166 @@
-
 // create by scratch3-extension generator
 const ArgumentType = require("../../extension-support/argument-type");
 const BlockType = require("../../extension-support/block-type");
+const Clone = require("../../util/clone");
 const formatMessage = require("format-message");
-const log = require('../../util/log');
+const fetchWithTimeout = require("../../util/fetch-with-timeout");
+const log = require("../../util/log");
 
 const menuIconURI = null;
 const blockIconURI = null;
 
-class testExt{
-  constructor (runtime){
-    this.runtime = runtime;
-    // communication related
-    this.comm = runtime.ioDevices.comm;
-    this.session = null;
-    this.runtime.registerPeripheralExtension('testExt', this);
-    // session callbacks
-    this.reporter = null;
-    this.onmessage = this.onmessage.bind(this);
-    this.onclose = this.onclose.bind(this);
-    this.write = this.write.bind(this);
-    // string op
-    this.decoder = new TextDecoder();
-    this.lineBuffer = '';
-  }
+/**
+ * How long to wait in ms before timing out requests to translate server.
+ * @type {int}
+ */
+const serverTimeoutMs = 10000; // 10 seconds (chosen arbitrarily).
 
-  onclose (){
-    this.session = null;
-  }
+const REMOTE_URL = {
+    UNIT: "/api/talk/classify",
+};
 
-  write (data, parser = null){
-    if (this.session){
-      return new Promise(resolve => {
-        if (parser){
-          this.reporter = {
-            parser,
-            resolve
-          }
-        }
-        this.session.write(data);
-      })
+class TextRecognition {
+    constructor(runtime) {
+        this.runtime = runtime;
+        // communication related
+        this.comm = runtime.ioDevices.comm;
+        this.session = null;
+        this.runtime.registerPeripheralExtension("diTextRecognition", this);
+        // session callbacks
+        this.reporter = null;
+        // string op
+        this.decoder = new TextDecoder();
+        this.lineBuffer = "";
     }
-  }
 
-  onmessage (data){
-    const dataStr = this.decoder.decode(data);
-    this.lineBuffer += dataStr;
-    if (this.lineBuffer.indexOf('\n') !== -1){
-      const lines = this.lineBuffer.split('\n');
-      this.lineBuffer = lines.pop();
-      for (const l of lines){
-        if (this.reporter){
-          const {parser, resolve} = this.reporter;
-          resolve(parser(l));
+    /**
+     * The key to load & store a target's pen-related state.
+     * @type {string}
+     */
+    static get STATE_KEY() {
+        return "Di.TextRecognition";
+    }
+    /**
+     * The default state, to be used when a target has no existing state.
+     * @type {HelloWorldState}
+     */
+    static get DEFAULT_IMAGERECOGNITION_STATE() {
+        return {
+            robotAnswer: "",
+            robotQuestion: "你好",
+            robotAnswerList: [],
         };
-      }
     }
-  }
 
-  scan (){
-    this.comm.getDeviceList().then(result => {
-        this.runtime.emit(this.runtime.constructor.PERIPHERAL_LIST_UPDATE, result);
-    });
-  }
+    get REMOTE_URL() {
+        return REMOTE_URL;
+    }
 
-  getInfo (){
-    return {
-      id: 'diIntelligentData',
-      name: 'Test',
-      color1: '#f5a623',
-      color2: '#235a4a',
-      menuIconURI: menuIconURI,
-      blockIconURI: blockIconURI,
-      blocks: [
-        {
-          opcode: 'a',
-          blockType: BlockType.COMMAND,
-          arguments: {
-            TXT: {
-              type: ArgumentType.STRING
-            },
-            X: {
-              type: ArgumentType.STRING
-            },
-            boolean: {
-              type: ArgumentType.BOOLEAN
-            }
-          },
-          text: 'block name label text [TXT] [X] [boolean]'
-        },
-        {
-          opcode: 'b',
-          blockType: BlockType.REPORTER,
-          arguments: {
-            TXT: {
-              type: ArgumentType.STRING
-            },
-            X: {
-              type: ArgumentType.STRING
-            },
-            boolean: {
-              type: ArgumentType.BOOLEAN
-            }
-          },
-          text: 'block name label text [TXT] [X] [boolean]'
-        },
-        {
-          opcode: 'c',
-          blockType: BlockType.BOOLEAN,
-          text: 'block name'
-        },
-        {
-          opcode: 'd',
-          blockType: BlockType.HAT,
-          isEdgeActivated: false,
-          text: 'block name'
+    /**
+     * @param {Target} target - collect  state for this target.
+     * @returns {HelloWorldState} the mutable state associated with that target. This will be created if necessary.
+     * @private
+     */
+    _getState(target) {
+        let state = target.getCustomState(TextRecognition.STATE_KEY);
+        if (!state) {
+            state = Clone.simple(
+                TextRecognition.DEFAULT_IMAGERECOGNITION_STATE
+            );
+            target.setCustomState(TextRecognition.STATE_KEY, state);
         }
-      ]
+        return state;
     }
-  }
 
-a (args, util){
-  const TXT = args.TXT;
-  const X = args.X;
-  const boolean = args.boolean;
+    /**
+     * Create data for a menu in scratch-blocks format, consisting of an array
+     * of objects with text and value properties. The text is a translated
+     * string, and the value is one-indexed.
+     * @param {object[]} info - An array of info objects each having a name
+     *   property.
+     * @return {array} - An array of objects with text and value properties.
+     * @private
+     */
+    _buildMenu(info) {
+        return info.map((entry, index) => {
+            const obj = {};
+            obj.text = entry.name;
+            obj.value = entry.value || String(index + 1);
+            return obj;
+        });
+    }
 
-  return this.write(`M0 \n`);
+    getInfo() {
+        return {
+            id: "diTextRecognition",
+            name: "智能数据",
+            color1: "#D8D8D8",
+            color2: "#009AFF",
+            color3: "#3373CC",
+            menuIconURI: menuIconURI,
+            blockIconURI: blockIconURI,
+            blocks: [
+                {
+                    opcode: "RobotAnswer",
+                    blockType: BlockType.REPORTER,
+                    text: formatMessage({
+                        id: "diTextRecognition.talkBack",
+                        default: "我的回答是[TXT]",
+                        description: "Robot saying",
+                    }),
+                    arguments: {
+                      TXT: {
+                          type: ArgumentType.IMAGE,
+                          defaultValue: formatMessage({
+                              id: "diTextRecognition.defaultSayWords",
+                              default: "你好",
+                              description: "default word",
+                          }),
+                      },
+                  },
+                },
+            ],
+        };
+    }
+
+    sayToRobot(args, util) {
+        const TXT = args.TXT;
+        const state = this._getState(util.target);
+        state.robotQuestion = TXT;
+        const generateAnswer = (list) => {
+            state.robotAnswer =
+                list[Math.floor(Math.random() * list.length)].say;
+        };
+        if (TXT) {
+            return new Promise((resolve, reject) => {
+                fetchWithTimeout(
+                    this.runtime.REMOTE_HOST + this.REMOTE_URL.UNIT,
+                    {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({
+                            terminal_id: this.runtime.uuid,
+                            query: state.robotQuestion,
+                        }),
+                    },
+                    serverTimeoutMs
+                )
+                    .then((response) => response.json())
+                    .then((data) => {
+                        state.robotAnswerList = data.data;
+                        generateAnswer(state.robotAnswerList);
+                        resolve();
+                    });
+            });
+        } else {
+            alert("请输入智能对话内容！");
+        }
+    }
+
+    RobotAnswer(args, util) {
+        const state = this._getState(util.target);
+        return state.robotAnswer;
+    }
 }
 
-b (args, util){
-  const TXT = args.TXT;
-  const X = args.X;
-  const boolean = args.boolean;
-
-  return this.write(`M0 \n`);
-}
-
-c (args, util){
-
-  return this.write(`M0 \n`);
-}
-
-d (args, util){
-
-  return this.write(`M0 \n`);
-}
-
-}
-
-module.exports = testExt;
+module.exports = TextRecognition;
