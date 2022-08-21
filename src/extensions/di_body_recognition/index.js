0fcf95322cbd5b028ce1635ff3d0ac71ef77854d
pan9 <438063442@qq.com>
2022-08-21 20:43:47 +0800

    v0.2.22

--- src/extensions/di_body_recognition/index.js
+++ src/extensions/di_body_recognition/index.js
@@ -507,6 +507,7 @@ class diBodyRecognition {
                     "POST",
                     this.runtime.REMOTE_HOST + this.REMOTE_URL.EMOTION
                 );
+                xhr.setRequestHeader("Access-Token", this.runtime.getToken());
                 xhr.send(form);
                 xhr.onreadystatechange = function () {
                     if (xhr.readyState == 4) {
@@ -545,6 +546,7 @@ class diBodyRecognition {
                     "POST",
                     this.runtime.REMOTE_HOST + this.REMOTE_URL.BODYNUM
                 );
+                xhr.setRequestHeader("Access-Token", this.runtime.getToken());
                 xhr.send(form);
                 xhr.onreadystatechange = function () {
                     if (xhr.readyState == 4) {
@@ -582,6 +584,7 @@ class diBodyRecognition {
                     "POST",
                     this.runtime.REMOTE_HOST + this.REMOTE_URL.CHARACTER
                 );
+                xhr.setRequestHeader("Access-Token", this.runtime.getToken());
                 xhr.send(form);
                 xhr.onreadystatechange = function () {
                     if (xhr.readyState == 4) {
@@ -624,6 +627,7 @@ class diBodyRecognition {
                     "POST",
                     this.runtime.REMOTE_HOST + this.REMOTE_URL.BODYAXES
                 );
+                xhr.setRequestHeader("Access-Token", this.runtime.getToken());
                 xhr.send(form);
                 xhr.onreadystatechange = function () {
                     if (xhr.readyState == 4) {
